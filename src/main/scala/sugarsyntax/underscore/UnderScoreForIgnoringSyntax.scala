03bb556adae80b1db4d82e52aa441c214c58c576
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2019-09-06 14:12:49 +0430

    underscore sugar syntax example in test for ignoring variable

---
+++ src/main/scala/sugarsyntax/underscore/UnderScoreForIgnoringSyntax.scala
@@ -0,0 +1,9 @@
+package sugarsyntax.underscore
+
+/**
+  * The sugarsyntax.underscore can be used to ignore things as: not used variables or not used types.
+  */
+object UnderScoreForIgnoringSyntax extends App {
+
+    println("See the test section of it")
+}
