2727dd57b54cb34e1942b89ad86cdee7faf12605
Ehsan Mozaffari <Ehsan.Mozaffari@SnappTrip.com>
2019-09-08 11:55:41 +0430

    Option test of filling null with null

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,16 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
-
 trait Greeting {
   lazy val greeting: String = "hello"
+}
+
+object Hello extends Greeting with App {
+//  println(greeting)
+
+  val strOption: Option[String] = None
+
+  val a: String = strOption.getOrElse("sdfsafd")
+//  val b: String = strOption.map(_)
+//  println(b)
+
 }
---
+++ src/test/scala/option/OptionSpec.scala
@@ -0,0 +1,26 @@
+package option
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+case class User(id: Int, firstName: String, lastName: String, age: Int, gender: Option[String])
+
+class OptionSpec extends FlatSpec with Matchers {
+  val SampleUser1 = User(1, "John", "Doe", 32, Some("male"))
+  val SampleUser2: Option[User] = Option(User(id=2,firstName = null, lastName = "Doe", age = 30, gender = None))
+
+  "Option" should
+  "Return None if null" in {
+
+    val testUser2 = SampleUser2
+    val a: Option[String] = testUser2.map(_.firstName)
+    val b: Option[String] = a.flatMap(Option(_))
+    val c: Option[Option[String]] = a.map(Option(_))
+
+    a shouldEqual Some(null)
+    b shouldEqual None
+    c shouldEqual Option(None)
+    c shouldEqual Some(None)
+
+  }
+}
