abacc413a71f098d924970a96a020637a081dd85
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2019-10-28 03:03:08 +0330

    add map and flatMap test for Option

--- src/test/scala/option/OptionSpec.scala
+++ src/test/scala/option/OptionSpec.scala
@@ -1,20 +1,22 @@
 package option
 
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers
+import org.scalatest.{FlatSpec, Matchers}
 
 case class User(id: Int, firstName: String, lastName: String, age: Int, gender: Option[String])
 
 class OptionSpec extends FlatSpec with Matchers {
   val SampleUser1 = User(1, "John", "Doe", 32, Some("male"))
-  val SampleUser2: Option[User] = Option(User(id=2,firstName = null, lastName = "Doe", age = 30, gender = None))
+
+  val SampleUser2: Option[User] = Option(
+    User(id = 2, firstName = null, lastName = "Doe", age = 30, gender = None)
+  )
 
   "Option" should
   "Return None if null" in {
 
     val testUser2 = SampleUser2
-    val a: Option[String] = testUser2.map(_.firstName)
-    val b: Option[String] = a.flatMap(Option(_))
+    val a: Option[String]         = testUser2.map(_.firstName)
+    val b: Option[String]         = a.flatMap(Option(_))
     val c: Option[Option[String]] = a.map(Option(_))
 
     val d: Option[String] = testUser2.map(_.firstName).flatMap(Option(_))
@@ -27,4 +29,39 @@ class OptionSpec extends FlatSpec with Matchers {
     d shouldEqual None
 
   }
+
+  "Option# Applying a method on Option" should "Return null if it is None and Some if is not None" in {
+
+    val testEmptyStringOption:    Option[String] = Option.empty
+    val testNonEmptyStringOption: Option[String] = Some("NonEmptyOption")
+
+    def testMethod(x:          String): String         = s"$x successful"
+    def testMethodRetOption(x: String): Option[String] = Some(s"$x successful")
+
+    testEmptyStringOption shouldBe None
+
+    testEmptyStringOption.map(testMethod) shouldBe None
+    testEmptyStringOption.flatMap(testMethodRetOption) shouldBe None
+
+    val badEmptyOptionCheck: Option[String] = testEmptyStringOption match {
+      case Some(x) => Some(testMethod(x))
+      case None    => None
+    }
+    badEmptyOptionCheck shouldBe None
+    testEmptyStringOption.map(testMethod) shouldBe badEmptyOptionCheck
+    testEmptyStringOption.flatMap(testMethodRetOption) shouldBe badEmptyOptionCheck
+
+    testNonEmptyStringOption shouldBe Some("NonEmptyOption")
+    testNonEmptyStringOption.map(testMethod) shouldBe Some("NonEmptyOption successful")
+    testNonEmptyStringOption.flatMap(testMethodRetOption) shouldBe Some("NonEmptyOption successful")
+
+    val badNonEmptyOptionCheck = testNonEmptyStringOption match {
+      case Some(x) => Some(testMethod(x))
+      case None    => None
+    }
+    badNonEmptyOptionCheck shouldBe Some("NonEmptyOption successful")
+    testNonEmptyStringOption.map(testMethod) shouldBe badNonEmptyOptionCheck
+    testNonEmptyStringOption.flatMap(testMethodRetOption) shouldBe badNonEmptyOptionCheck
+  }
+
 }
