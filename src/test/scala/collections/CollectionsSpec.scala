d5eeba6efaca9fd4121c42b14d8dbb689ce1a175
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2021-04-09 01:53:54 +0430

    Add test of Set and Map and ArraySeq

--- src/test/scala/collections/CollectionsSpec.scala
+++ src/test/scala/collections/CollectionsSpec.scala
@@ -1,7 +1,11 @@
 package collections
 
+import java.util.NoSuchElementException
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+
+import scala.collection.mutable
 
 class CollectionsSpec extends AnyFlatSpec with should.Matchers {
 
@@ -17,4 +21,60 @@ class CollectionsSpec extends AnyFlatSpec with should.Matchers {
 
     result shouldEqual "A,B,C,D"
   }
+
+  "Immutable Set" should
+    "add and see the overlaps the variables" in {
+
+    val set1: Set[Int] = Set(1, 2, 3, 4, 5, 5, 5, 6, 7, 7, 8, 9)
+    val set2: Set[Int] = Set(3, 3, 4, 5, 10)
+
+    set1 should have size 9
+    set2 should have size 4
+    set1 & set2 shouldBe Set(3, 4, 5)
+    set1 ++ set2 shouldBe Set(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    set1.head should not equal 1 // Think about it
+    //set1.tail should not equal set1 - set1.head is not equal always
+    set2 + 6 shouldBe Set(3, 4, 5, 6, 10)
+    set1(5) shouldBe true
+    set1(10) shouldBe false
+    set1(5) shouldBe set1.contains(5)
+
+  }
+
+  "Immutable Map" should
+    "works as expected" in {
+    val mappedHotelById = Map(1 -> "Espinas", (2, "Behrood"), 2 -> "Khalijefars", 2 -> "Esteghlal", 3 -> "Hilton", 3 -> "Morvarid")
+
+    mappedHotelById.get(3) shouldBe Some("Morvarid")
+    mappedHotelById.get(2) shouldBe Some("Esteghlal")
+    mappedHotelById.head shouldBe(1, "Espinas")
+    mappedHotelById.head shouldBe(1, "Espinas")
+    mappedHotelById(3) shouldBe "Morvarid"
+    an[NoSuchElementException] should be thrownBy mappedHotelById(5)
+    the[NoSuchElementException] thrownBy mappedHotelById(5) should have message "key not found: 5"
+  }
+
+  "array" should
+    "be mutable all the time and works as expected" in {
+
+    val bags: Array[AnyVal] = Array(1, true, 1f)
+    val fruits: Array[String] = Array("apple", "banana", "cherry", "banana")
+    val fruitsBananaToRaspberry: Unit = fruits.update(1, "raspberry")
+
+    fruits(0) shouldBe "apple"
+    fruits(1) should not equal "banana"
+    fruits(1) shouldBe "raspberry"
+    fruits(0) shouldBe fruits.apply(0)
+    //    fruits(0) = "raspberry" shouldBe "raspberry"
+  }
+
+  "mutable array" should
+    "be mutable as scala 2.13" in {
+
+    val mutableArray = mutable.ArraySeq(1, 2, 3)
+    mutableArray.update(0, 10)
+    mutableArray(0) shouldBe 10
+  }
+
+
 }
