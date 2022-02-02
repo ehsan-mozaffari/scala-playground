65407026a7bb8e2c1bece515af0727f2ed0465b3
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2022-02-02 17:11:48 +0330

    add type annotation

--- src/test/scala/collections/LazyListSpec.scala
+++ src/test/scala/collections/LazyListSpec.scala
@@ -8,7 +8,7 @@ class LazyListSpec extends AnyFlatSpec with Matchers {
   "LazyList" should
     "be defined via #:: and LazyList.empty" in {
 
-      val testLazyList = 1 #:: 2 #:: 3 #:: 4 #:: LazyList.empty
+      val testLazyList: LazyList[Int] = 1 #:: 2 #:: 3 #:: 4 #:: LazyList.empty
 
       testLazyList.toString shouldBe "LazyList(<not computed>)"
     }
